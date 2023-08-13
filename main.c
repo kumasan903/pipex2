f1d7b59e8b3fe8442cbc0a28562e7ad0805693c6
skawanis <skawanis@student.42tokyo.jP>
2023-08-13 19:26:21 +0900

    コマンドを同時に実行するように変更

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 21:25:46 by skawanis          #+#    #+#             */
-/*   Updated: 2023/08/06 21:54:13 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/08/13 19:24:47 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #define PIPE_READ 0
 #define PIPE_WRITE 1
 
-void	execute_cmd(char *cmd, char **envp, int in_fd, int out_fd)
+pid_t	execute_cmd(char *cmd, char **envp, int in_fd, int out_fd)
 {
 	char	**new_cmd;
 	char	**path;
@@ -31,15 +31,17 @@ void	execute_cmd(char *cmd, char **envp, int in_fd, int out_fd)
 	pid = fork();
 	if (pid > 0)
 	{
-		wait(NULL);
 		free(binary_path);
 		free_all((void **)new_cmd, 0);
+		return(pid);
 	}
 	if (pid == 0)
 	{
 		if (execve(binary_path, new_cmd, envp))
 			perror("pipex");
+		return (0);
 	}
+	return (1);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -47,14 +49,17 @@ int	main(int argc, char **argv, char **envp)
 	int	infile_fd;
 	int	outfile_fd;
 	int	my_pipe[2];
+	pid_t	pid[2];
 
 	if (pipe(my_pipe))
 		exit (1);
 	handle_arguments(argc, argv, &infile_fd, &outfile_fd);
 	if (infile_fd >= 0)
-		execute_cmd(argv[2], envp, infile_fd, my_pipe[PIPE_WRITE]);
+		pid[0] = execute_cmd(argv[2], envp, infile_fd, my_pipe[PIPE_WRITE]);
 	else
 		close(my_pipe[PIPE_WRITE]);
-	execute_cmd(argv[3], envp, my_pipe[PIPE_READ], outfile_fd);
+	pid[1] = execute_cmd(argv[3], envp, my_pipe[PIPE_READ], outfile_fd);
+	waitpid(pid[0], NULL, WNOHANG);
+	waitpid(pid[1], NULL, WNOHANG);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: skawanis <skawanis@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 21:25:51 by skawanis          #+#    #+#             */
-/*   Updated: 2023/08/06 16:33:38 by skawanis         ###   ########.fr       */
+/*   Updated: 2023/08/13 19:24:59 by skawanis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
-void	execute_cmd(char *cmd, char **envp, int in_fd, int out_fd);
+pid_t	execute_cmd(char *cmd, char **envp, int in_fd, int out_fd);
 void	handle_arguments(int argc, char **argv, int *in_fd, int *out_fd);
 char	**get_path(char **envp);
 char	*search_binary(char **path, char *cmd_name);
